13daec84a6a6415cad6e4be95f23796af51fc356
Kateryna Rublenko <metronavt94@gmail.com>
2022-05-04 22:45:24 +0300

    added api keys

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,7 @@ import { setNews } from "./index";
 
 export const getNews = () => async (dispatch) => {
   const response = await axios.get(
-    `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=e61c439194f54519b9d9123ee438a160`
   );
   dispatch(setNews(response.data.articles));
 };
