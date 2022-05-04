9744b331b5e79eb77bb8f16e20c09e7c0b8028a7
Kateryna Rublenko <metronavt94@gmail.com>
2022-05-04 23:24:09 +0300

    api keys moved to .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyASkvtR_OMZ1wz5OMb-yWthyTiWesHugGA",
-  authDomain: "auth-4283a.firebaseapp.com",
-  projectId: "auth-4283a",
-  storageBucket: "auth-4283a.appspot.com",
-  messagingSenderId: "133281709240",
-  appId: "1:133281709240:web:175bbfb03b5c534ec3884d",
-});
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
 
-export const auth = app.auth();
-export default app;
+export const auth = app.auth()
+export default app
